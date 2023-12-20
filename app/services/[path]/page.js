26b82b3a694a7e66d4cd817bc5a8045107ab609f
Abhishek <abhisheklal0095@gmail.com>
2023-12-21 02:03:11 +0530

    fetching static pages from db"

--- app/services/[path]/page.js
+++ app/services/[path]/page.js
@@ -1,12 +1,22 @@
+import { PathnameContext } from 'next/dist/shared/lib/hooks-client-context.shared-runtime'
 import React from 'react'
+export const dynamicParams = false
+
 
 
 export async function generateStaticParams() {
-  return [
-      { path: 'path1'},
-      { path: 'path2'}
-  ]
+    // API call to get all the categories
+  const categories = await fetch(`http://${process.env.API_URL}/api/best-of-categories?populate=*`)
+  const categoriesJson = await categories.json()
+  const data = categoriesJson.data
+  console.log(data)
+  const paths = data.map((service) => {
+    return { path: service.attributes.path }
+  })
+  console.log(paths)
+  return paths
 }
+
  
 
 
