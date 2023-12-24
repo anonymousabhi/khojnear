a0b1daf00ba7ea02f53d13a35c4a3b8c568b587c
Ankit Kumar <ankit.k@alshaya.com>
2023-12-24 22:28:48 +0530

    add error handling

--- app/services/[path]/page.js
+++ app/services/[path]/page.js
@@ -1,26 +1,29 @@
-import { PathnameContext } from 'next/dist/shared/lib/hooks-client-context.shared-runtime'
 import React from 'react'
+
 export const dynamicParams = false
 
 
-
 export async function generateStaticParams() {
-    // API call to get all the categories
-  const categories = await fetch(`http://${process.env.API_URL}/api/best-of-categories?populate=*`)
-  const categoriesJson = await categories.json()
-  const data = categoriesJson.data
-  console.log(data)
-  const paths = data.map((service) => {
-    return { path: service.attributes.path }
-  })
-  console.log(paths)
-  return paths
+  try {
+    const categories = await fetch(`http://${process.env.API_URL}/api/best-of-categories?populate=*`)
+    const categoriesJson = await categories.json()
+    const data = categoriesJson.data
+    const paths = data.map((service) => {
+      return { params: { path: service.attributes.path } }
+    })
+    return paths
+  } catch (error) { 
+    console.log(error)
+    return [
+      { path: '' }
+    ]
+  }
 }
 
- 
-
-
 export default function Page({ params }) {
-  const {path} = params
-  return <div>Category {path}</div>
+  return (
+    <div>
+      <h1>{params.path}</h1>
+    </div>
+  )
 }
