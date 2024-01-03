58ba8c08649141d5b7f176178e7d41b6f0c8018c
Abhishek <abhisheklal0095@gmail.com>
2024-01-03 22:33:44 +0530

    adding home page

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css'
+import Link from 'next/link';
 
 export const metadata = {
   title: 'Khoj Near',
@@ -8,7 +9,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+      <header>
+        <nav>
+          <Link href="/">Home</Link>
+          <Link href="/about">About Us</Link>
+        </nav>
+      </header>
+      <div>{children}</div>
+      </body>
     </html>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,9 +1,28 @@
-import Image from 'next/image'
+'use client'
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [coordinates, setCoordinates] = useState(null); 
+  const [error, setError] = useState(null); 
+  
+  const handleGetLocation = () => { 
+    navigator.geolocation.getCurrentPosition( 
+    ({ coords }) => { 
+      setCoordinates(coords); 
+      setError(null);
+      console.log(coords);
+      },
+      (error) => { 
+        setError(error.message); 
+        console.log("Failed to get location!");
+      } 
+    );
+  };
+
   return (
     <>
-    
+    HOME PAGE
     </>
   )
 }
